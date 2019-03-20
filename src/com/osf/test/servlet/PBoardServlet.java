5914c7b4306e9f13f45721233a68a1559ecd49ee
jkj113 <kumjoo_92@naver.com>
2019-03-20 17:21:20 +0900

    3월20일

---
+++ src/com/osf/test/servlet/PBoardServlet.java
@@ -0,0 +1,65 @@
+package com.osf.test.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+public class PBoardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	private static String savePath = "D:\\study\\workspace\\osf-jsp\\WebContent\\upload";
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		uri = uri.replace("/pboard/", "");
+	    if("insert".equals(uri)) {
+	    	DiskFileItemFactory dfiFactory = new DiskFileItemFactory();
+	    	String tmpPath = System.getProperty("java.io.tmpdir");
+	    	File tmpFile = new File(tmpPath);
+	    	dfiFactory.setRepository(tmpFile);
+//    	dfiFactory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+	    	dfiFactory.setSizeThreshold(10*1024*1024); //얼마나 저장할지 정해준다.(바이트 단위/보통 1MB를 1000KB로 계산한다.)
+	    	
+	    	ServletFileUpload sfu = new ServletFileUpload(dfiFactory); //파싱하기 위한 객채
+	    	sfu.setSizeMax(20*1024*1024); //여기도 바이트 -> 전체 사이즈 / 사이즈는 서버 생각하면서 해야지?
+	    	sfu.setFileSizeMax(20*1024*1024); //-> 파일 하나 사이즈 /우리는 파일 하나니까 같이 맞춰도 괜찮다.
+	    	try {
+				List<FileItem> fileList = sfu.parseRequest(request);
+				for(int i = 0; i<fileList.size();i++) {
+					FileItem fi = fileList.get(i);
+				if(fi.isFormField()) {
+					
+				}else {
+					String fileName = fi.getName();
+					File saveFile = new File(savePath+"\\"+fileName);
+					fi.write(saveFile);
+				}
+				}
+			} catch (FileUploadException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	    }else if("update".equals(uri)) {
+	    	
+	    }else if("delete".equals(uri)) {
+	    	
+	    }else {
+	    	
+	    }
+	}
+
+}

d31ba98243aeefdb02cad88cdfa131297424bb9c
mm6861 <woohoomi@nate.com>
2019-03-21 17:50:15 +0900

    3-21

--- src/com/osf/test/servlet/PBoardServlet.java
+++ src/com/osf/test/servlet/PBoardServlet.java
@@ -2,8 +2,11 @@ package com.osf.test.servlet;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,15 +17,36 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+import com.osf.test.service.PBoardService;
+import com.osf.test.service.impl.PBoardServiceImpl;
+
 public class PBoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	private static String savePath = "D:\\study\\workspace\\osf-jsp\\WebContent\\upload";
-	//나중에 할 때는 이 경로가 필요없다. 톰캣에서 제공하는 리얼 경로를 사용한다.
+	private PBoardService pbs = new PBoardServiceImpl();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String uri = request.getRequestURI();
+		uri = uri.replace("/pboard/", "");
+		if("list".equals(uri)) {
+			request.setAttribute("pBoardList", pbs.selectPBoardList());
+			RequestDispatcher rd = request.getRequestDispatcher("/views/photo-board/list.jsp");
+			rd.forward(request, response);
+			return;
+		}else {
+			try {				
+				int pbNum = Integer.parseInt(uri);
+				System.out.println(pbs.selectPBoard(pbNum));
+				request.setAttribute("pBoard", pbs.selectPBoard(pbNum));
+				RequestDispatcher rd = request.getRequestDispatcher("/views/photo-board/view.jsp");
+				rd.forward(request, response);
+				return;
+			}catch(NumberFormatException e) {
+				throw new ServletException("상세조회는 번호조회만 가능합니다. ");
+			}
+		}
 	}
-
+ 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String uri = request.getRequestURI();
 		uri = uri.replace("/pboard/", "");
@@ -31,23 +55,36 @@ public class PBoardServlet extends HttpServlet {
 	    	String tmpPath = System.getProperty("java.io.tmpdir");
 	    	File tmpFile = new File(tmpPath);
 	    	dfiFactory.setRepository(tmpFile);
-//    	dfiFactory.setRepository(new File(System.getProperty("java.io.tmpdir")));
-	    	dfiFactory.setSizeThreshold(10*1024*1024); //얼마나 저장할지 정해준다.(바이트 단위/보통 1MB를 1000KB로 계산한다.)
+	    	dfiFactory.setSizeThreshold(10*1024*1024); 
 	    	
-	    	ServletFileUpload sfu = new ServletFileUpload(dfiFactory); //파싱하기 위한 객채
-	    	sfu.setSizeMax(20*1024*1024); //여기도 바이트 -> 전체 사이즈 / 사이즈는 서버 생각하면서 해야지?
-	    	sfu.setFileSizeMax(20*1024*1024); //-> 파일 하나 사이즈 /우리는 파일 하나니까 같이 맞춰도 괜찮다.
+	    	ServletFileUpload sfu = new ServletFileUpload(dfiFactory); 
+	    	sfu.setSizeMax(20*1024*1024); 
+	    	sfu.setFileSizeMax(20*1024*1024); 
 	    	try {
 				List<FileItem> fileList = sfu.parseRequest(request);
+				Map<String,String> pBoard = new HashMap<>();
 				for(int i = 0; i<fileList.size();i++) {
 					FileItem fi = fileList.get(i);
 				if(fi.isFormField()) {
-					
+					if(fi.isFormField()) {
+						pBoard.put(fi.getFieldName(), fi.getString("utf-8"));
+					}
 				}else {
-					String fileName = fi.getName();
-					File saveFile = new File(savePath+"\\"+fileName);
+					String rfileName = fi.getName();
+					String extName = rfileName.substring(rfileName.lastIndexOf(".")+1);
+					String fileName = System.currentTimeMillis()+"";
+					File saveFile = new File(savePath+"\\"+fileName+"."+extName);
+					pBoard.put("pb_file_path", rfileName);
+					pBoard.put("pb_file_path", "/uploas/" + fileName + "." +extName);
 					fi.write(saveFile);
 				}
+				}
+				if(pbs.insertPBoard(pBoard)==1) {
+					request.setAttribute("msg", "집중해라!");
+					request.setAttribute("url", "/views/photo-board/insert.jsp");
+					RequestDispatcher rd = request.getRequestDispatcher("/views/result.jsp");
+					rd.forward(request, response);
+					return;
 				}
 			} catch (FileUploadException e) {
 				e.printStackTrace();
@@ -61,6 +98,11 @@ public class PBoardServlet extends HttpServlet {
 	    }else {
 	    	
 	    }
+	}
+	public static void main(String[] args) {
+		String fileName = "채희주.흑역사.jpg";
+		System.out.println(fileName.lastIndexOf("."));
+			
 	}
 
 }
